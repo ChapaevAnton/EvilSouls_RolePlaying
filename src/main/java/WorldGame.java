f87073467866fb1cc4f5d2a4980b30b6562f9640
ChapaevAnton <virsus2009@gmail.com>
2021-04-15 11:36:22 +0700

    add logo for game

--- src/main/java/WorldGame.java
+++ src/main/java/WorldGame.java
@@ -2,16 +2,30 @@ import units.FightUnit;
 import units.Hero;
 import units.Skeleton;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WorldGame {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+
+        // FIXME: 15.04.2021 доработать загрузку лого
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/logo.txt"));
+        String line;
+        while ((line=bufferedReader.readLine())!=null) {
+            System.out.println("\u001B[31m"+line);
+        }
 
         FightUnit player = new Hero("player", 5, 5);
         FightUnit skeleton = new Skeleton("skeleton", 5, 100);
 
 
         new Battlefield().battle(player,skeleton);
+
+
 
     }
 
