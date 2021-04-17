6c53bad55ab23b91ee4bd480b09760b9b682b463
Chapaev Anton <virsus2009@gmail.com>
2021-04-17 16:40:25 +0700

    in the process of development the main menu of the program
    *todo check pls man

--- src/main/java/WorldGame.java
+++ src/main/java/WorldGame.java
@@ -1,11 +1,8 @@
-import battle.BattleCallback;
-import battle.Battlefield;
 import units.FightUnit;
-import units.GenerateUnits;
-import units.Hero;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,43 +12,66 @@ public class WorldGame {
 
     public static FightUnit player;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         //загрузка логотипа
         final Path logo = Paths.get("src/main/resources/logo.txt");
         final Path logoPath = logo.toAbsolutePath();
 
-        try (BufferedReader input = Files.newBufferedReader(logoPath, StandardCharsets.UTF_8)) {
+        try (BufferedReader loadLogo = Files.newBufferedReader(logoPath, StandardCharsets.UTF_8)) {
             String line;
-            while ((line = input.readLine()) != null) {
+            while ((line = loadLogo.readLine()) != null) {
                 System.out.println("\u001B[31m" + line);
             }
         } catch (IOException err) {
             err.printStackTrace();
         }
 
+        // TODO: 17.04.2021 разработка главного меню игры
 
+        //enter для продолжения
+        String selected;
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        selected = console.readLine();
 
+        System.out.println("Добро пожаловать в ад!!!\n1. Создать героя\n2. Выход\nВведите команду");
 
-        player = new Hero("player", 5, 5);
-
-        FightUnit skeleton = GenerateUnits.getFightUnit(player.getLevel());
-
-        System.out.println(player.getFullInfoUnit());
-        System.out.println(skeleton.getFullInfoUnit());
-
-        new Battlefield().battle(player, skeleton, new BattleCallback() {
-            @Override
-            public void battleWin() {
-
+        selected = console.readLine();
+        //главное меню программы
+        switch (selected) {
+            case "1" -> {
+                System.out.println("Создание героя...");
             }
 
-            @Override
-            public void battleLos() {
-                System.out.println("GAME OVER");
+            case "2" -> {
+                System.out.println("Работа программы завершена...");
             }
-        });
+
+            default -> {
+                System.out.println("Несуществующая команда, повторите ввод...");
+            }
+        }
+
+
+//        player = new Hero("player", 5, 5);
+//
+//        FightUnit skeleton = GenerateUnits.getFightUnit(player.getLevel());
+//
+//        System.out.println(player.getFullInfoUnit());
+//        System.out.println(skeleton.getFullInfoUnit());
+//
+//        new Battlefield().battle(player, skeleton, new BattleCallback() {
+//            @Override
+//            public void battleWin() {
+//
+//            }
+//
+//            @Override
+//            public void battleLos() {
+//                System.out.println("GAME OVER");
+//            }
+//        });
 
     }
 
